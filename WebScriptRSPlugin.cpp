a4c69e4ade536e37bc03948fa39590abf345f9a2
Chozabu <chozabu@gmail.com>
2013-12-13 14:17:11 +0000

    Change back to old/new way of getting svn version to match recent RS update

--- WebScriptRSPlugin.cpp
+++ WebScriptRSPlugin.cpp
@@ -25,7 +25,7 @@ extern "C" {
 	// It will be tested by RS to load the plugin automatically, since it is safe to load plugins
 	// with same revision numbers, assuming that the revision numbers are up-to-date.
 	//
-    uint32_t RETROSHARE_PLUGIN_revision = RsUtil::retroshareRevision();// SVN_REVISION_NUMBER ;
+    uint32_t RETROSHARE_PLUGIN_revision = SVN_REVISION_NUMBER ;
 
 	// This symbol contains the svn revision number grabbed from the executable. 
 	// It will be tested by RS to load the plugin automatically, since it is safe to load plugins
@@ -40,7 +40,7 @@ void WebScriptRSPlugin::getPluginVersion(int& major,int& minor,int& svn_rev) con
 {
 	major = 5 ;
 	minor = 4 ;
-    svn_rev = RsUtil::retroshareRevision();//SVN_REVISION_NUMBER ;
+    svn_rev = SVN_REVISION_NUMBER ;
 }
 
 WebScriptRSPlugin::WebScriptRSPlugin()
