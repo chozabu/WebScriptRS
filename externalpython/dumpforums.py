b9241c605e68a24893bda4f176f58f9399801090
Chozabu <chozabu@gmail.com>
2014-01-02 16:31:59 +0000

    export names instead of IDs in RPC forum dumper

--- externalpython/dumpforums.py
+++ externalpython/dumpforums.py
@@ -2,22 +2,33 @@
 
 import jsonrpc
 import socket
-
-
-rpc = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(), jsonrpc.TransportSocket(addr="/tmp/testservice", sock_type=socket.AF_UNIX))
-
 import random
 import json
-#import PyQt4
-print ""
-#print 'names in current namespace', dir()
+import sys
+
 print "dumping subscribed forums... please wait!"
+rpc = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(), jsonrpc.TransportSocket(addr="/tmp/testservice", sock_type=socket.AF_UNIX))
+print "loading forums..."
 forums = rpc.bridge.getForums()
+print "loading keyring..."
+gpgIdList = rpc.bridge.getGPGAllList()
+print "gen name hash..."
+gpgIds = {}
+for i in gpgIdList:
+  n = rpc.bridge.getPeerName(i)
+  print n
+  gpgIds[i]=n
+print "writing forums list..."
+
 with open('../jout/forums.json', 'w') as ffile:
     ffile.write(json.dumps(forums))
 def dumpMsg(forumID,msgID):
             #print msgID
             msg = rpc.bridge.getForumMessage(forumID,msgID)
+            sys.stdout.write('   '+msg['srcId']+': ')
+            if msg['srcId'] != '':
+              msg['srcId'] = gpgIds[msg['srcId']]
+              sys.stdout.write(msg['srcId']+' ')
             with open("../jout/msg"+msgID+".json", 'w') as ffile:
                 ffile.write(json.dumps(msg))
             msgheaders = rpc.bridge.getForumThreadMsgList(forumID,msgID)
@@ -25,6 +36,7 @@ def dumpMsg(forumID,msgID):
                 ffile.write(json.dumps(msgheaders))
             for msgh in msgheaders:
                 dumpMsg(forumID, msgh['msgId'])
+print "writing forums..."
 for f in forums:
     if f['forumRelationship'] == "SUBSCRIBED":
         print "dumping " + f['forumId']
